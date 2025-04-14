78361100550941f914c8ba0406929db55702f0c6
Alex <alex70.07@mail.ru>
2025-04-14 19:51:57 +0300

    Разделил и настроил urls проекта и приложения

--- barbershop/urls.py
+++ barbershop/urls.py
@@ -1,28 +1,31 @@
 """
-URL configuration for barbershop project.
+Настройка URL-адресов для проекта barbershop.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+Список urlpatterns связывает URL-адреса с представлениями. Подробнее см. документацию:
+https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Примеры:
+
+Для view-функций:
+1. Импортируйте view: from my_app import views
+2. Добавьте URL в urlpatterns: path('', views.home, name='home')
+
+Для view-классов:
+1. Импортируйте view: from other_app.views import Home
+2. Добавьте URL в urlpatterns: path('', Home.as_view(), name='home')
+
+Подключение другого URL-конфига:
+1. Импортируйте функцию include(): from django.urls import include, path
+2. Добавьте URL в urlpatterns: path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import landing, thanks, orders_list, order_detail
+from django.urls import include
+from core.views import landing
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', landing, name='landing'),
-    path('thanks/', thanks, name='thanks'),
-    path('orders/', orders_list, name='orders_list'),
-    path('orders/<int:order_id>/', order_detail, name='order_detail')
+    # Подключаем маршруты из приложения core
+    path('barbershop/', include('core.urls')),
 ]
---
+++ core/urls.py
@@ -0,0 +1,10 @@
+# core/urls.py
+from django.urls import path
+from core.views import landing, thanks, orders_list, order_detail
+
+# Эти маршруты будут доступны с префиксом /barbershop/
+urlpatterns = [
+    path('thanks/', thanks, name='thanks'),
+    path('orders/', orders_list, name='orders_list'),
+    path('orders/<int:order_id>/', order_detail, name='order_detail'),
+]
