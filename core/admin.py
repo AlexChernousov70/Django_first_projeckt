197f06ccf56edf4a478e472f2edd8f89e98a3461
Alex <alex70.07@mail.ru>
2025-04-20 16:19:42 +0300

    hw_39 Создание модели Order, регистрация в админке

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Order
 
-# Register your models here.
+
+# Регистрация в одну строку
+admin.site.register(Order)
+# admin.site.register(Master)
+# admin.site.register(Service)
+# admin.site.register(Review)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    """
+    Класс = таблица в БД, атрибуты = поля в таблице
+    """
+    # Статусы заказов
+    STATUS_CHOICES = [
+        ("not_approved", "Не подтвержден"),
+        ("moderated", "Прошел модерацию"),
+        ("spam", "Спам"),
+        ("approved", "Подтвержден"),
+        ("in_awaiting", "В ожидании"),
+        ("completed", "Завершен"),
+        ("canceled", "Отменен"),
+    ]
+
+    client_name = models.CharField(max_length=100, verbose_name="Имя клиента")
+    phone = models.CharField(max_length=20, verbose_name="Телефон")
+    comment = models.TextField(blank=True, verbose_name="Комментарий")
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default="not_approved", verbose_name="Статус") # определили строго определенный набор значений
+    date_created = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    date_updated = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
+    # Один ко многим
+    # master = models.ForeignKey("Master", on_delete=models.SET_NULL, null=True, related_name="orders")
+    appointment_date = models.DateTimeField(blank=True, null=True, verbose_name="Дата и время записи")
+
+    class Meta:
+        """
+        Используется для определения метаданных модели, для каждой модели свой класс Meta
+        """
+        verbose_name = "Заказ" # verbose_name - задаёт удобное для человека название модели в единственном числе. Используется в админ-панели Django и других местах интерфейса.
+        verbose_name_plural = "Заказы" # verbose_name_plural - задаёт название модели во множественном числе. Если не указать, Django автоматически добавит "s" к verbose_name.
+        ordering = ['-appointment_date'] # определяет порядок сортировки объектов модели по умолчанию. В данном случае ['-appointment_date'] означает сортировку по полю appointment_date в обратном порядке (от новых к старым).
+
+    def __str__(self):
+        return f"Заказ #{self.id} - {self.client_name} ({self.get_status_display()})"
