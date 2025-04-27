c20e2a42df69d2077ce39bfbf66f6ea41518c4f5
Alex <alex70.07@mail.ru>
2025-04-27 11:08:01 +0300

    hw_40 Добавлена строка поиска, в разработке

--- core/views.py
+++ core/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from .data import *
 from .models import Order, Master, Service, Review
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+
 
 def landing(request):
     context = {
@@ -23,11 +25,62 @@ def thanks(request):
 def orders_list(request):
 
     orders = Order.objects.all().order_by('-date_created') # получение всех объектов, сортированных по дате создания
+
+    if request.method == "GET":
+        # Получаем все заказы
+        # Используем жадную загрузку для мастеров и услуг
+        # all_orders = Order.objects.prefetch_related("master", "services").all()
+        # all_orders = Order.objects.all()
+        all_orders = Order.objects.select_related("master").prefetch_related("services").all()
+    
+        # Получаем строку поиска
+        search_query = request.GET.get("search", None)
+
+        if search_query:
+            # Получаем чекбоксы
+            check_boxes = request.GET.getlist("search_in")
+
+            # Проверяем Чекбоксы и добавляем Q объекты в запрос
+            # |= это оператор "или" для Q объектов
+            filters = Q()
+
+            if "phone" in check_boxes:
+                # Полная запись где мы увеличиваем фильтры
+                filters = filters | Q(phone__icontains=search_query)
+
+            if "name" in check_boxes:
+                # Сокращенная запись через inplace оператор
+                filters |= Q(client_name__icontains=search_query)
+            
+            if "comment" in check_boxes:
+                filters |= Q(comment__icontains=search_query)
+
+            if filters:
+                # Если фильтры появились. Если Q остался пустым, мы не попадем сюда
+                all_orders = all_orders.filter(filters)
+
     context = {
         'orders': orders,
         'is_orders_list': True
     }
     return render(request, 'core/orders_list.html', context)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @login_required
 def order_detail(request, order_id):
