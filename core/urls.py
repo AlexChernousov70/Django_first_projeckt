5f9442bd538cb77121f65998746ebeba9017c540
Alex <alex70.07@mail.ru>
2025-05-02 22:25:26 +0300

    hw_41 Добавили путь для создания в urls чтобы отобразить форму для создания услуг

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 # core/urls.py
 from django.urls import path
-from .views import thanks, orders_list, order_detail
+from .views import thanks, orders_list, order_detail, service_create
 
 # Эти маршруты будут доступны с префиксом /barbershop/
 urlpatterns = [
     path('thanks/', thanks, name='thanks'),
     path('orders/', orders_list, name='orders_list'),
     path('orders/<int:order_id>/', order_detail, name='order_detail'),
+    path('services/create/', service_create, name='service_create'),
 ]
