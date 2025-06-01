2b5d0875ba20e257a3d587c0846d9b0db8beb6bc
Alex <alex70.07@mail.ru>
2025-06-01 12:40:53 +0300

    hw_43 фикс конфликта зависимостей

--- core/telegram_bot.py
+++ core/telegram_bot.py
@@ -1,18 +1,17 @@
 import os
 import logging
-import telegram # pip install python-telegram-bot
+from telegram import Bot# pip install python-telegram-bot
 import asyncio
 from dotenv import load_dotenv
 # Загрузка переменных окружения из .env файла
 load_dotenv()
-
 
 # Настройка логирования
 logging.basicConfig(level=logging.DEBUG)
 
 async def send_telegram_message(token, chat_id, message, parse_mode="Markdown"):
     try:
-        bot = telegram.Bot(token=token)
+        bot = Bot(token=token)
         await bot.send_message(chat_id=chat_id, text=message, parse_mode=parse_mode)
         logging.info(f'Сообщение "{message}" отправлено в чат {chat_id}')
     except Exception as e:
