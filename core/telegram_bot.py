05d575b90079b0ee5411d6f3aa1a024aee1f3b20
Alex <alex70.07@mail.ru>
2025-06-01 12:07:39 +0300

    hw_43 удалил лишний пакет, добавил напоминалку для запуска проверки бота

--- core/telegram_bot.py
+++ core/telegram_bot.py
@@ -19,10 +19,10 @@ async def send_telegram_message(token, chat_id, message, parse_mode="Markdown"):
         logging.error(f"Ошибка отправки сообщения в чат {chat_id}: {e}")
         raise
 
-# Тестируем отправку прямо тут
+# Тестируем отправку прямо тут запуск командой poetry run python core\telegram_bot.py иначе не запустится
 if __name__ == "__main__":
     load_dotenv()
     TELEGRAM_BOT_API_KEY = os.getenv("TELEGRAM_BOT_API_KEY")
     TELEGRAM_USER_ID = os.getenv("TELEGRAM_USER_ID")
-    message = "Тестовое сообщение"
+    message = "*я тебя люблю*"
     asyncio.run(send_telegram_message(TELEGRAM_BOT_API_KEY, TELEGRAM_USER_ID, message))
