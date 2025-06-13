ed23375371aa4126ea5098925b5e5c10989d5d05
Alex <alex70.07@mail.ru>
2025-06-13 19:30:11 +0300

    fix убираем лишние файлы и убираю ошибку в настройках

--- barbershop/settings.py
+++ barbershop/settings.py
@@ -25,9 +25,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=$6)yrs^+!=d3*7z465tn9v-8s!2arbdca=)r7r4jyi$co8qso'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
