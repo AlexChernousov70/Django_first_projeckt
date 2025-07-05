5d0790ef6246c3ecab7bee102dbba9f80b45c3c6
Alex <alex70.07@mail.ru>
2025-07-05 19:16:01 +0300

    hw_46 Разработал классовые представления списков заказов и услуг

--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,14 @@
 # core/urls.py
 from django.urls import path
-from .views import ThanksView, orders_list, order_detail, service_list, service_create, create_review, thanks_for_the_review, order_create, get_master_info, masters_services_by_id
+from .views import ThanksView, ServiceListView, OrdersListView, order_detail, service_create, create_review, thanks_for_the_review, order_create, get_master_info, masters_services_by_id
 
 # Эти маршруты будут доступны с префиксом /barbershop/
 urlpatterns = [
 
     path('thanks/', ThanksView.as_view(), name="thanks"),
-    path('orders/', orders_list, name='orders_list'),
+    path('orders/', OrdersListView.as_view(), name='orders_list'),
     path('orders/<int:order_id>/', order_detail, name='order_detail'),
-    path('services/', service_list, name='service_list'),
+    path('services/', ServiceListView.as_view(), name='service_list'),
     path('services/create/', service_create, name='service_create'),
     path('review/create/', create_review, name='create_review'),
     path('thanks/review/', thanks_for_the_review, name='thanks_for_the_review'),
--- core/views.py
+++ core/views.py
@@ -7,7 +7,7 @@ from django.db.models import Q
 from django.contrib import messages
 from .forms import ServiceForm, ReviewForm, OrderForm
 import json
-
+from django.contrib.auth.mixins import LoginRequiredMixin # Миксин для ограничения доступа к странице
 from django.views.generic import TemplateView, ListView
 
 class LandingPageView(TemplateView):
@@ -30,29 +30,35 @@ class ThanksView(TemplateView):
         context['masters_count'] = Master.objects.count()
         return context
 
-
-
-@login_required
-def orders_list(request):
-    orders = Order.objects.select_related("master").prefetch_related("services").order_by('-date_created')
+class OrdersListView(LoginRequiredMixin, ListView):
+    model = Order
+    template_name = 'core/orders_list.html'
+    context_object_name = 'orders' # Автоматически добавит 'orders' в контекст
+    ordering = ['-date_created'] # По умолчанию будет использоваться queryset из model
     
-    search_query = request.GET.get("search")
-    if search_query:
-        search_fields = request.GET.getlist("search_in")
-        filters = Q()
-        if "phone" in search_fields:
-            filters |= Q(phone__icontains=search_query)
-        if "name" in search_fields:
-            filters |= Q(client_name__icontains=search_query)
-        if "comment" in search_fields:
-            filters |= Q(comment__icontains=search_query)
-        orders = orders.filter(filters) if filters else orders.none()
-
-    context = {
-        'orders': orders,
-        'is_orders_list': True
-    }
-    return render(request, 'core/orders_list.html', context)
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.select_related("master").prefetch_related("services") # Добавляем prefetch_related для оптимизации запросов
+        
+        # Добавляем фильтрацию по полям
+        search_query = self.request.GET.get("search") # Получаем значение из GET-параметра 'search'
+        if search_query: 
+            search_fields = self.request.GET.getlist("search_in")
+            filters = Q()
+            if "phone" in search_fields:
+                filters |= Q(phone__icontains=search_query)
+            if "name" in search_fields:
+                filters |= Q(client_name__icontains=search_query)
+            if "comment" in search_fields:
+                filters |= Q(comment__icontains=search_query)
+            queryset = queryset.filter(filters) if filters else queryset.none()
+        
+        return queryset
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_orders_list'] = True
+        return context
 
 @login_required
 def order_detail(request, order_id):
@@ -69,13 +75,15 @@ def order_detail(request, order_id):
     }
     return render(request, 'core/order_detail.html', context)
 
-def service_list(request):
-    services = Service.objects.all()
-    context = {
-        'title': 'Услуги',
-        'services': services,
-    }
-    return render(request, 'core/service_list.html', context)
+class ServiceListView(ListView):
+    model = Service
+    template_name = 'core/service_list.html'
+    context_object_name = 'services' # Автоматически добавит 'services' в контекст
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs) # Получаем стандартный контекст от ListView, например 'object_list'
+        context['title'] = 'Услуги' # Добавляем свои данные в контекст
+        return context
 
 def service_create(request):
     if request.method == "GET":
